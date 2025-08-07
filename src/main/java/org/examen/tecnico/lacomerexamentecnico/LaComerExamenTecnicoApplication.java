309ff5d3f289099d94fc198453d31f567c359f17
David Carrillo <david.carrillo@criteria.mx>
2025-08-07 15:46:31 -0600

    Punto entrada springboot

---
+++ src/main/java/org/examen/tecnico/lacomerexamentecnico/LaComerExamenTecnicoApplication.java
@@ -0,0 +1,13 @@
+package org.examen.tecnico.lacomerexamentecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaComerExamenTecnicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaComerExamenTecnicoApplication.class, args);
+    }
+
+}
