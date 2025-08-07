c532a7a81fa7e3e7c294daa8daac2206e9765fea
David Carrillo <david.carrillo@criteria.mx>
2025-08-07 15:48:39 -0600

    Interaccion JPA para las busquedas, creaciones, eliminaciones y más de los registros

---
+++ src/main/java/org/examen/tecnico/lacomerexamentecnico/repository/DireccionRepository.java
@@ -0,0 +1,7 @@
+package org.examen.tecnico.lacomerexamentecnico.repository;
+
+import org.examen.tecnico.lacomerexamentecnico.model.Direccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DireccionRepository extends JpaRepository<Direccion, Long> {
+}
